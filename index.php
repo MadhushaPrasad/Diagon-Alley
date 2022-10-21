2911ec1a046ab9d2889f2f5cf7e7025f7530f203
Dhanuka Lakshan <danuka1999lakshan@gmail.com>
2022-10-21 18:55:27 +0530

    create layout

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="startup.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+
+    <title>Home</title>
 </head>
+
 <body>
-    
+<div class="row">
+    <div class="col-lg-6">
+        
+    </div>
+    <div class="col-lg-6">
+        <img src="" alt="" srcset="">
+    </div>
+</div>
 </body>
+
 </html>
