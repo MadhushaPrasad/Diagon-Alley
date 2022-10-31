00abee60f816d2eb466bcdf94c3ae9926d9a5b7b
Shalini Muthumali Athukorala <99632841+IT21169076@users.noreply.github.com>
2022-10-31 17:20:57 +0530

    navbar (#56)

---
+++ components/navbar.php
@@ -0,0 +1,37 @@
+<link href="navbar.css" rel="stylesheet">
+<header class="header">
+
+    <h2 class="logo">
+        <span class='bi bi-ladder' style='color:#ff7800'></span> Diagon alley </h2>
+
+    <nav class="navbar">
+        <a href="..">Home</a>
+        <a href="..">Books</a>
+        <a href="..">Feedback</a>
+        <a href="..">About Us</a>
+        <a href="..">Contact Us</a>
+    </nav>
+
+    <div class="icons">
+        
+        <button id="search-btn"><a href="..">Sign in</a></button>
+        <button id="login-btn"><a href="..">Register</a></button>
+        <div class="fas fa-bars" id="menu-btn"></div>
+    
+    </div>
+    <div class="MENU">
+        <button id="login-btn"><a href="..">Home</a></button>
+        <div class="box">
+            <a href="..">Books</a> 
+        </div>
+        <div class="box">
+            <a href="..">Feedback</a>
+        </div>
+        <div class="box">
+            <a href="..">About Us</a>
+        </div> 
+        <div class="box">
+            <a href="..">Contact Us</a>
+        </div>
+    </div>
+</header>
