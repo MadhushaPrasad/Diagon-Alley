5bb77223c83a00600aab395d2e21c44aec4aa2f9
Dhanuka Lakshan <danuka1999lakshan@gmail.com>
2022-10-31 23:56:50 +0530

    #signup php file (#63)
    * create startup page index
    
    * create layout
    
    * start up
    
    * start up update
    
    * new update
    
    * start up up to date
    
    * start up up to date
    
    * start up up to date
    
    * home page completed
    
    * home page completed
    
    * Home Page - Hero Section #22 Completed
    
    * #1 Armstrong task completed
    
    * #2 swap number
    
    * #1 register1
    
    * #2 register completed
    
    * ]
    
    * Delete register2.html
    
    * Delete SwapNumbers.java
    
    * Delete Armstrong.java
    
    * #1 new register 1
    
    * #2 new register 2
    
    * # signup php file
    
    * #2 index php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <link href="letestbook.css" rel="stylesheet">
+<center><div class ="tite">
+    <h1><label class="title">latest Books</label></h1><br><br>
+</div></center><br>
+
+<div class="book">
+<div class=" reg">
+<img class="card-img" src="1.jpg">
+<h2>Java overload method </h2>
+</div>
+
+
+    
+<div class=" reg">
+<img class="card-img" src="../Diagon-Alley/assets/images/41i9SH8xRqL.AC_SY780.jpg">
+<h2>Java overload method</h2>
+</div>
+    
+<div class=" reg">
+<img class="card-img" src="../Diagon-Alley/assets/images/41i9SH8xRqL.AC_SY780.jpg">
+<h2>Java overload method</h2>
+</div>
+
+<div class=" reg">
+<img class="card-img" src="../Diagon-Alley/assets/images/41i9SH8xRqL.AC_SY780.jpg">
+<h2>Java overload method </h2>
+</div>
+    
+<div class=" reg">
+<img class="card-img" src="../Diagon-Alley/assets/images/41i9SH8xRqL.AC_SY780.jpg">
+<h2>Java overload method</h2>
+</div>
+    
+<div class=" reg">
+<img class="card-img" src="../Diagon-Alley/assets/images/41i9SH8xRqL.AC_SY780.jpg">
+<h2>Java overload method</h2>
+</div>
+</div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>  
+<br>  
+<form>  
+  
+
+Course :  
+</label>   
+<select>  
+<option value="Course">Course</option>  
+<option value="BCA">BCA</option>  
+<option value="BBA">BBA</option>  
+<option value="B.Tech">B.Tech</option>  
+<option value="MBA">MBA</option>  
+<option value="MCA">MCA</option>  
+<option value="M.Tech">M.Tech</option>  
+</select>  
+  
+<br>  
+<br>  
+<label>   
+Gender :  
+</label><br>  
+<input type="radio" name="male"/> Male <br>  
+<input type="radio" name="female"/> Female <br>  
+<input type="radio" name="other"/> Other  
+<br>  
+<br>  
+  
+<label>   
+Phone :  
+</label>  
+<input type="text" name="country code"  value="+91" size="2"/>   
+<input type="text" name="phone" size="10"/> <br> <br>  
+Address  
+<br>  
+<textarea cols="80" rows="5" value="address">  
+</textarea>  
+<br> <br>  
+Email:  
+<input type="email" id="email" name="email"/> <br>    
+<br> <br>  
+Password:  
+<input type="Password" id="pass" name="pass"> <br>   
+<br> <br>  
+Re-type password:  
+<input type="Password" id="repass" name="repass"> <br> <br>  
+<input type="button" value="Submit"/>  
+</form>  
+</body>
+</html>
+ 
