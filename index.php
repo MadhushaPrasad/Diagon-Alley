17e7825b8c2166c64c84184aa5881f7ac23ae542
MadhushaPrasad <madushaprasad21@gmail.com>
2023-10-03 15:04:46 +0530

    remove wrong things and add .gitignore file

--- index.php
+++ index.php
@@ -1,50 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>home</title>
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/Banner-Heading-Image-images.css">
+    <link rel="stylesheet" href="../assets/css/Navbar-Right-Links-icons.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
 </head>
+
 <body>
-    <link href="letestbook.css" rel="stylesheet">
-<center><div class ="tite">
-    <h1><label class="title">latest Books</label></h1><br><br>
-</div></center><br>
-
-<div class="book">
-<div class=" reg">
-<img class="card-img" src="1.jpg">
-<h2>Java overload method </h2>
-</div>
-
-
-    
-<div class=" reg">
-<img class="card-img" src="../Diagon-Alley/assets/images/41i9SH8xRqL.AC_SY780.jpg">
-<h2>Java overload method</h2>
-</div>
-    
-<div class=" reg">
-<img class="card-img" src="../Diagon-Alley/assets/images/41i9SH8xRqL.AC_SY780.jpg">
-<h2>Java overload method</h2>
-</div>
-
-<div class=" reg">
-<img class="card-img" src="../Diagon-Alley/assets/images/41i9SH8xRqL.AC_SY780.jpg">
-<h2>Java overload method </h2>
-</div>
-    
-<div class=" reg">
-<img class="card-img" src="../Diagon-Alley/assets/images/41i9SH8xRqL.AC_SY780.jpg">
-<h2>Java overload method</h2>
-</div>
-    
-<div class=" reg">
-<img class="card-img" src="../Diagon-Alley/assets/images/41i9SH8xRqL.AC_SY780.jpg">
-<h2>Java overload method</h2>
-</div>
-</div>
+    <section class="py-4 py-xl-5">
+        <div class="container">
+            <div class="bg-dark border rounded border-0 border-dark overflow-hidden">
+                <div class="row g-0">
+                    <div class="col-md-6">
+                        <div class="text-white p-4 p-md-5">
+                            <h5>Biography</h5>
+                            <h2 class="fw-bold text-white mb-3" style="padding-top: 0px;margin-right: 3px;margin-bottom: -12px;padding-bottom: 0px;">Who We Are<br></h2>
+                            <p class="mb-4"><br>Lorem Media is a full-service social media agency. We offer businesses innovative solutions that deliver the right type of audience to you in the most effective strategies possible. We strive to develop a community around your business, polishing your branding, and improving your public relations.<br>Social Media is now one of the most powerful marketing tools with the ability to communicate with a target audience in real time.<br>It's 2019: time to sink or swim.<br>We are your Social Media Marketing Agency.<br><br></p>
+                            <div class="my-3"><a class="btn btn-primary btn-lg me-2" role="button" href="#" style="--bs-primary: E1AE2A;--bs-primary-rgb: 225,174,42;--bs-body-bg: #E1AE2A;"><strong>See More</strong></a></div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 order-first order-md-last" style="min-height: 250px;"><img class="w-100 h-100 fit-cover" src="assets/img/young%20people%20looking%20at%20laptop.svg"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
+
 </html>
