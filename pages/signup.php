ef62cfc3aa9c754260a12c97ff654740e5e386f7
Sandaru-Dev <sandaru334@gmail.com>
2023-10-10 12:42:49 +0530

    Enhance code readability and performance

--- pages/signup.php
+++ pages/signup.php
@@ -13,33 +13,31 @@
     <div class="container">
         <div class="component-1">
             <img id="logo" src="../assets/img/logo.svg" alt="logo" width="182px" height="40px" >
-           
             <h2>Get Started!</h2>
             <h4>Please sign up to continue</h4>
-
             <div class="form">
-                <label for="">Email</label>
-                <input type="email" name="" id="" placeholder="mail@example.com">
+                <label for="email">Email</label>
+                <input type="email" name="email"  id="email" placeholder="mail@example.com">
 
-                <label for="">User Name</label>
-                <input type="text" name="" id="" placeholder="madhush99">
+                <label for="user-name">User Name</label>
+                <input type="text" name="user-name" id="user-name" placeholder="madhush99">
 
-                <label for="">Password</label>
-                <input type="password" name="" id="" placeholder="********">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="********">
 
-                <label for="">Confirm Password</label>
-                <input type="password" name="" id="" placeholder="********">
+                <label for="confirm-password">Confirm Password</label>
+                <input type="password" name="confirm-password" id="confirm-password" placeholder="********">
             </div>
 
             <div class="position">
-                <input type="radio" name="position" id="">
+                <input type="radio" name="position">
                 <label for="Student">Student</label>
-                <input type="radio" name="position" id="">
+                <input type="radio" name="position">
                 <label for="Student">Admin</label>
             </div>
-            
+
             <div class="terms-condition">
-                <input type="checkbox" name="" id="">
+                <input type="checkbox" name="accept-terms">
                 <p>I accept <a href="#">Terms and Conditions</a></p>
             </div>
 
@@ -51,18 +49,18 @@
             </div>
 
             <div class="social-media">
-                <a href="#"> <img src="../assets/img/google-icon.png" alt="">Sign up with Google</a>
-                <a href="#"><img src="../assets/img/facebook-icon.png" alt=""> Sign up with Facebook</a>
+                <a href="#"> <img src="../assets/img/google-icon.png" alt="Google icon">Sign up with Google</a>
+                <a href="#"><img src="../assets/img/facebook-icon.png" alt="Facebook icon"> Sign up with Facebook</a>
             </div>
 
             <div class="already">
                  <p>Already user? <a href="#">Login</a></p>
             </div>
         </div>
-            
+
         <div class="component-2">
         <div class="position-fixed">
-            <img src="../assets/img/young couple sitting on the floor and reading books.svg" alt="couple image" width="550px">
+            <img src="../assets/img/young couple sitting on the floor and reading books.svg" alt="young couple sitting on the floor and reading books" width="550px">
             <div class="text-content">
                 <h4>Books are a uniquely portable magic.</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae cras at</p>
