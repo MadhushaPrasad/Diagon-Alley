90f4ac7ae6873b2ac5c8467b6e2f41e0875864a6
Janani-Balasooriya <jananidev7@gmail.com>
2023-10-13 17:55:14 +0530

    Implement Contact Us component

--- components/contactus.php
+++ components/contactus.php
@@ -1,32 +1,36 @@
-<link href="contactus.css" rel="stylesheet">
-<div class="container contact-form">
-    <form method="post">
-        <h3>Send us message </h3>
-       <div class="row">
-            <div class="col-md-6">
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="contactus.css" rel="stylesheet">
+</head>
+<body>
+    <div class="contact-form">
+        <form method="post">
+            <h3>Send us a message</h3>
+            <div class="form-row">
                 <div class="form-group">
-                    <input type="text" name="txtName" class="form-control" placeholder="First Name" value="" />
-                </div><br>
+                    <input type="text" name="txtFirstName" class="form-control" placeholder="First Name" value="" />
+                </div>
                 <div class="form-group">
-                    <input type="text" name="txtName" class="form-control" placeholder="Last Name" value="" />
-                </div><br>
+                    <input type="text" name="txtLastName" class="form-control" placeholder="Last Name" value="" />
+                </div>
+            </div>
+            <div class="form-row">
                 <div class="form-group">
                     <input type="text" name="txtPhone" class="form-control" placeholder="Mobile Number" value="" />
-                </div><br>
-                <div class="form-group">
-                    <input type="text" name="txtemail" class="form-control" placeholder="Email" value="" />
-                </div><br>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <textarea name="txtemsg" class="form-control" placeholder="message" style="width: 100%; height: 150px;"></textarea>
-                    </div><br>
                 </div>
-                 </div>
+                <div class="form-group">
+                    <input type="text" name="txtEmail" class="form-control" placeholder="Email" value="" />
+                </div>
+            </div>
             <div class="form-group">
+                <textarea name="txtMsg" class="form-control" placeholder="Message"></textarea>
+            </div>
+            <div class="form-group text-center">
                 <input type="submit" name="btnSubmit" class="btnContact" value="Send" />
             </div>
-            
-            </div>
-        </div>
-    </form>
-</div>
+        </form>
+    </div>
+</body>
+</html>
