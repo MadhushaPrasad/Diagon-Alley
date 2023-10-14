d8338ea0f58d32973824690d293af2add1f30056
Janani Balasooriya <94961626+Janani-Balasooriya@users.noreply.github.com>
2023-10-14 07:53:03 +0530

    Implemented Responsive Contact Us Component (#74)
    * Implement Contact Us component
    
    * Add validation to Contact Us component

--- components/contactus.php
+++ components/contactus.php
@@ -1,32 +1,55 @@
-<link href="contactus.css" rel="stylesheet">
-<div class="container contact-form">
-    <form method="post">
-        <h3>Send us message </h3>
-       <div class="row">
-            <div class="col-md-6">
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="contactus.css" rel="stylesheet">
+</head>
+<body>
+    <div class="contact-form">
+        <form method="post" onsubmit="return validateForm();">
+            <h3>Send us a message</h3>
+            <div class="form-row">
                 <div class="form-group">
-                    <input type="text" name="txtName" class="form-control" placeholder="First Name" value="" />
-                </div><br>
-                <div class="form-group">
-                    <input type="text" name="txtName" class="form-control" placeholder="Last Name" value="" />
-                </div><br>
-                <div class="form-group">
-                    <input type="text" name="txtPhone" class="form-control" placeholder="Mobile Number" value="" />
-                </div><br>
-                <div class="form-group">
-                    <input type="text" name="txtemail" class="form-control" placeholder="Email" value="" />
-                </div><br>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <textarea name="txtemsg" class="form-control" placeholder="message" style="width: 100%; height: 150px;"></textarea>
-                    </div><br>
+                    <input type="text" name="txtFirstName" class="form-control" placeholder="First Name" value="" required />
                 </div>
-                 </div>
+                <div class="form-group">
+                    <input type="text" name="txtLastName" class="form-control" placeholder="Last Name" value="" required />
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group">
+                    <input type="text" name="txtPhone" class="form-control" placeholder="Mobile Number" value="" required pattern="[0-9]{10}" />
+                    <span class="error-msg" id="phone-error"></span>
+                </div>
+                <div class="form-group">
+                    <input type="text" name="txtEmail" class="form-control" placeholder="Email" value="" required pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}" />
+                    <span class="error-msg" id="email-error"></span>
+                </div>
+            </div>
             <div class="form-group">
+                <textarea name="txtMsg" class="form-control" placeholder="Message" required></textarea>
+            </div>
+            <div class="form-group text-center">
                 <input type="submit" name="btnSubmit" class="btnContact" value="Send" />
             </div>
-            
-            </div>
-        </div>
-    </form>
-</div>
+        </form>
+    </div>
+    <script>
+        function validateForm() {
+            document.getElementById('phone-error').textContent = '';
+            document.getElementById('email-error').textContent = '';
+            const phoneNumber = document.querySelector('input[name="txtPhone"]').value;
+            if (!/^[0-9]{10}$/.test(phoneNumber)) {
+                document.getElementById('phone-error').textContent = 'Invalid phone number format';
+                return false;
+            }
+            const email = document.querySelector('input[name="txtEmail"]').value;
+            if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) {
+                document.getElementById('email-error').textContent = 'Invalid email format';
+                return false;
+            }
+            return true;
+        }
+</script>
+</body>
+</html>
