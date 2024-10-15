b046f2f430db55a8e8ed76da10c6d8771467d11e
Biswajit Sarkar <biswajitsarkar0485@gmail.com>
2024-10-15 09:32:28 +0530

    feat: add responsive footer with logo, links, and contact section (#102)
    Co-authored-by: biswa <biswa@007gmail.com>

--- components/contactus.php
+++ components/contactus.php
@@ -26,7 +26,43 @@
             <input type="submit" name="btnSubmit" class="btnContact" value="Send" />
         </div>
     </form>
-</div>
+    </div>
+
+    <!-- Footer Section -->
+<footer>
+  <div class="footer-container"  >
+    <div class="footer-logo">
+      <img src="../assets/img/logo.svg" alt="Diagon Alley Logo">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit nunc facilisis amet sed justo pellentesque. Enim congue nunc at auctor et leo, diam faucibus tellus.</p>
+    </div>
+
+    <div class="footer-links">
+      <h4>Quick Links</h4>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Books</a></li>
+        <li><a href="#">Feedback</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+    </div>
+
+    <div class="footer-terms">
+      <h4>Terms and Conditions</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit nunc facilisis amet sed justo pellentesque. Enim congue nunc at auctor et leo, diam faucibus tellus.</p>
+    </div>
+
+    <div class="footer-contact"  >
+      <h4>Contact</h4>
+      <ul>
+        <li><i class="fa fa-phone"></i> 000-000-000</li>
+        <li><i class="fa fa-envelope"></i> info@email.com</li>
+        <li><i class="fa fa-map-marker"></i> Kalutara South</li>
+      </ul>
+    </div>
+  </div>
+</footer>
+
 
 <script>
     function validateForm() {
@@ -47,3 +83,4 @@
 </script>
 
 <link href="contactus.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
--- index.php
+++ index.php
@@ -29,6 +29,7 @@
             </div>
         </div>
     </section>
+ 
     <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
