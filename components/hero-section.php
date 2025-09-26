d714a21a853bd140c14e8a24252562fdd42ced58
Madhusha Prasad <madushaprasad21@gmail.com>
2025-09-26 08:25:13 +0530

    chore: update hero section

--- components/hero-section.php
+++ components/hero-section.php
@@ -1,7 +1,7 @@
 <div class="align-items-center justify-content-center w-100 row">
   <div class="gap-3 d-grid ps-5 col">
-    <H1>We Help People to get level up their tech knowledge</H1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, iste sint! Ipsam itaque molestiae magnam, quos ducimus assumenda quia saepe dolorem rerum accusamus eligendi blanditiis optio perspiciatis quaerat consequuntur voluptates.</p>
+    <H1 class="text-wrap fs-1 fw-bold">We Help People to get level up their tech knowledge</H1>
+    <p class="text-wrap fs-5 fst-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, iste sint! Ipsam itaque molestiae magnam, quos ducimus assumenda quia saepe dolorem rerum accusamus eligendi blanditiis optio perspiciatis quaerat consequuntur voluptates.</p>
     <div>
       <button class="rounded-3 text-white btn btn-warning" type="button">Tour</button>
     </div>
