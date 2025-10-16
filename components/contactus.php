119323818eef24ed28dfa7e346ebae1bdcaf2e02
Madhusha Prasad <madushaprasad21@gmail.com>
2025-10-16 07:49:47 +0530

    chore: Separate footer component from contact us #112

--- components/contactus.php
+++ components/contactus.php
@@ -28,41 +28,6 @@
     </form>
     </div>
 
-    <!-- Footer Section -->
-<footer>
-  <div class="footer-container"  >
-    <div class="footer-logo">
-      <img src="../../assets/img/logo.svg" alt="Diagon Alley Logo">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit nunc facilisis amet sed justo pellentesque. Enim congue nunc at auctor et leo, diam faucibus tellus.</p>
-    </div>
-
-    <div class="footer-links">
-      <h4>Quick Links</h4>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Books</a></li>
-        <li><a href="#">Feedback</a></li>
-        <li><a href="#">About Us</a></li>
-        <li><a href="#">Contact Us</a></li>
-      </ul>
-    </div>
-
-    <div class="footer-terms">
-      <h4>Terms and Conditions</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit nunc facilisis amet sed justo pellentesque. Enim congue nunc at auctor et leo, diam faucibus tellus.</p>
-    </div>
-
-    <div class="footer-contact"  >
-      <h4>Contact</h4>
-      <ul>
-        <li><i class="fa fa-phone"></i> 000-000-000</li>
-        <li><i class="fa fa-envelope"></i> info@email.com</li>
-        <li><i class="fa fa-map-marker"></i> Kalutara South</li>
-      </ul>
-    </div>
-  </div>
-</footer>
-
 
 <script>
     function validateForm() {
